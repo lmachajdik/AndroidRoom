73c10598654c25c15340492b650e2bb240bb60b4
Lukáš Machajdík <lukaskuko9@gmail.com>
2020-12-02 13:43:36 +0100

    added word entity and word dao

---
+++ app/src/main/java/com/example/androidroom/Word.kt
@@ -0,0 +1,14 @@
+package com.example.androidroom
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "word_table")
+class Word(
+
+    @PrimaryKey(autoGenerate = true) val id: Int,
+
+    @ColumnInfo(name = "word") val word: String
+
+)
